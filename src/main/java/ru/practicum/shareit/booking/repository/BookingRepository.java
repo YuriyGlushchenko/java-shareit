230d1a9904ffab9dd84d9d5d80400826c7c713dc
Yuriy Glushchenko <yuriygl86@gmail.com>
2026-01-28 18:03:34 +0300

    убрал лишние методы после QueryDSL

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -12,61 +12,11 @@ import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long>, QuerydslPredicateExecutor<Booking> {
 
-    List<Booking> findByBookerIdOrderByStartDesc(Long bookerId);
-
-    List<Booking> findByBookerIdAndStartBeforeAndEndAfterOrderByStartDesc(
-            Long bookerId, LocalDateTime now1, LocalDateTime now2);
-
-    List<Booking> findByBookerIdAndEndBeforeOrderByStartDesc(
-            Long bookerId, LocalDateTime now);
-
-    List<Booking> findByBookerIdAndStartAfterOrderByStartDesc(
-            Long bookerId, LocalDateTime now);
-
-    List<Booking> findByBookerIdAndStatusOrderByStartDesc(
-            Long bookerId, Status status);
-
-
-    List<Booking> findByItemOwnerIdOrderByStartDesc(Long ownerId);
-
-    List<Booking> findByItemOwnerIdAndStartBeforeAndEndAfterOrderByStartDesc(
-            Long ownerId, LocalDateTime now1, LocalDateTime now2);
-
-    List<Booking> findByItemOwnerIdAndEndBeforeOrderByStartDesc(
-            Long ownerId, LocalDateTime now);
-
-    List<Booking> findByItemOwnerIdAndStartAfterOrderByStartDesc(
-            Long ownerId, LocalDateTime now);
-
-    List<Booking> findByItemOwnerIdAndStatusOrderByStartDesc(
-            Long ownerId, Status status);
-
-
-    Booking findFirstByItemIdAndStatusAndEndBeforeOrderByEndDesc(
-            Long itemId,
-            Status status,
-            LocalDateTime now
-    );
-
-    Booking findFirstByItemIdAndStatusAndStartAfterOrderByStartAsc(
-            Long itemId,
-            Status status,
-            LocalDateTime now
-    );
-
     boolean existsByBookerIdAndItemIdAndStatusAndEndBefore(
             Long bookerId,
             Long itemId,
             Status status,
             LocalDateTime now
-    );
-
-    boolean existsByStartBetweenOrEndBetween(
-            LocalDateTime start,
-            LocalDateTime end,
-            LocalDateTime start1,
-            LocalDateTime end1
-
     );
 
     @Query("""
